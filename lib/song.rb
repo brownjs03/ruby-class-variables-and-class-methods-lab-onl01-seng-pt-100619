6e7dbb2f8d5524cae3da5b4a264e7fd297a3f582
Jeffrey Brown <brownjs03@gmail.com>
2019-11-01 21:30:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,11 +31,10 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist]
-        binding.pry
-        artist_count[artist] += 1 
-      else
+      if artist_count[artist] = 0
         artist_count[artist] = 1
+      else
+        artist_count[artist] += 1
       end
     end
     artist_count
