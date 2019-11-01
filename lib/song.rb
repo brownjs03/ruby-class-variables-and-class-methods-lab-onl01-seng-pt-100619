151351e83128c04368facbac50ece9e3a599553e
Jeffrey Brown <brownjs03@gmail.com>
2019-11-01 21:26:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist]
+      if artist_count[artist] = true
         artist_count[artist] += 1 
       else
         artist_count[artist] = 1
